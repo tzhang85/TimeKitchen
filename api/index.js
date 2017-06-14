7d69f3f162018c545cffea80e533d97512995c9d
Peilin Li <peilinli524@gmail.com>
2017-06-14 23:09:38 +0800

    database setup done

--- api/index.js
+++ api/index.js
@@ -1,23 +1,47 @@
 import express from 'express';
-import data from '../src/testData';
+import {MongoClient} from 'mongodb';
+import assert from 'assert'
+import config from '../config'
+
+let mdb;
+
+MongoClient.connect(config.mongodbUrl, (err, db) => {
+  assert.equal(null, err);
+
+  mdb = db;
+});
+
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
 
 
 router.get('/contests', (req, res) => {
-  res.send({
-    contests : contests
-  });
+  let contests = {};
+  mdb.collection('contests').find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1
+    })
+    .each((err, contest) => {
+      assert.equal(null, err);
+
+      if(!contest){
+        res.send({contests});
+        return;
+      }
+
+      contests[contest.id] = contest;
+    })
+
 });
 
 router.get('/contests/:contestId', (req, res) => {
   // req.params.contestId
-  let contest = contests[req.params.contestId];
-  res.send(contest);
+  mdb.collection('contests')
+    .findOne({id: Number(req.params.contestId)})
+    .then(contest => res.send(contest))
+    .catch(console.error)
 });
 
 export default router;
